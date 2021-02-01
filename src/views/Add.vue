ad492d74a8aaae33e6e9ca66ee6c0746e6f4e4f7
ichigo-dev <ichigo@web-freework.com>
2021-02-01 11:15:14 +0900

    Add.vue フォーム表示

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="about">
     <h1>add todo page.</h1>
-    <input type="text">
-    <button>add</button>
+    <b-form-group>
+    <b-row class='mt-4'>
+    <b-col sm='6' offset='3'>
+    <b-form-input type="text"></b-form-input>
+    </b-col>
+    <b-col sm='1'>
+    <b-button>add</b-button>
+    </b-col>
+    </b-row>
+    </b-form-group>
   </div>
 </template>
